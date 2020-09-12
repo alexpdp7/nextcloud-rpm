a2099711dbe1b3bcc72c4e683e3febba6bbf5849
alex <alex@pdp7.net>
2020-09-12 11:43:42 +0200

    Allow and document building sources or everything

--- build.sh
+++ build.sh
@@ -7,4 +7,4 @@ docker run -it \
 	-w /root/rpmbuild/SOURCES/ \
 	-v $(pwd)/SRPMS:/root/rpmbuild/SRPMS/ \
 	-v $(pwd)/RPMS:/root/rpmbuild/RPMS/ \
-	build:latest rpmbuild -ba nextcloud.spec
+	build:latest rpmbuild $1 nextcloud.spec
